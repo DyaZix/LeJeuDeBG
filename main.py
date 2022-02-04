251db07bdab2108c33e791b23dfdc18d217220d9
DyaZix-Code <79577721+DyaZix-Code@users.noreply.github.com>
2022-02-04 20:19:06 +0100

    comments

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ from pygame.locals import *
 from mapHandler import *
 import sys
  
+ 
+ 
+#pygame init
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((1920, 1080), DOUBLEBUF)    #set the display mode, window title and FPS clock
 pygame.display.set_caption('Le jeu bg')
 FPSCLOCK = pygame.time.Clock()
 
 
-
+#map init
 map_init()
 map_data = getMap()             
 displayMap(DISPLAYSURF)
