4d0acb26afe1a67b786af49cfed23b6304d76e43
DyaZix-Code <79577721+DyaZix-Code@users.noreply.github.com>
2022-02-05 11:02:45 +0100

    tout

--- main.py
+++ main.py
@@ -1,40 +1,93 @@
 import pygame
 from pygame.locals import *
 from mapHandler import *
+from camera import *
 import sys
 
- 
- 
-#pygame init
-pygame.init()
-WINDOW = pygame.display.set_mode((1920, 1080), DOUBLEBUF)    #set the display mode, window title and FPS clock
-pygame.display.set_caption('Le jeu bg')
-FPSCLOCK = pygame.time.Clock()
 
+ 
+class Game:
+    def __init__(self):
+        
+        self.runningState = True
+        
+        #pygame init
+        pygame.init()
+        pygame.display.set_caption('Le jeu bg')
+        self.WINDOW   = pygame.display.set_mode((1920, 1080), DOUBLEBUF)    #set the display mode, window title and FPS clock
+        self.FPSCLOCK = pygame.time.Clock()
+        
+        #camera init
+        self.camera = Camera()
+        
+        #map init
+        self.map = mapHandler()
+        self.map.map_areas_init()
+        self.map_data = self.map.getMap() 
+        self.map.loadTextures()            
+        self.map.displayMap(self.WINDOW, self.camera)
+        self.map.printMap()
+        
+        
+        
+        #music init & start
+        music = 'terraria.mp3'
+        pygame.init()
+        pygame.mixer.init()
+        pygame.mixer.music.load(music)
+        pygame.mixer.music.play(-1)     # If the loops is -1 then the music will repeat indefinitely.
+        pygame.mixer.music.set_volume(0.06)
+      
+    def main(self):
+        while self.isRunning():
+            
+            #updates gamestate
+            self.update()
+            
+            #game exit handle
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    self.runningState = False
+                    pygame.quit()
+                    sys.exit()
+                if event.type == KEYUP:
+                    if event.key == K_ESCAPE:
+                        self.runningState = False
+                        pygame.quit()
+                        sys.exit()
 
-#map init
-map_init()
-map_data = getMap() 
-loadTextures()            
-displayMap(WINDOW)
+            #clock
+            self.FPSCLOCK.tick(60)
+        
+    def update(self):
+        self.movement()
+        self.draw()
 
-#music init & start
-file = 'terraria.mp3'
-pygame.init()
-pygame.mixer.init()
-pygame.mixer.music.load(file)
-pygame.mixer.music.play(-1)     # If the loops is -1 then the music will repeat indefinitely.
-pygame.mixer.music.set_volume(0.06)
+        
+    def draw(self):
+        
+        #sets the map textures to the surface (screen)
+        self.map.displayMap(self.WINDOW, self.camera)
+        
+        #displays the surface content
+        pygame.display.flip()
+        
+    def movement(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_s]:
+            self.camera.setPosition(self.camera.getXPosition(), self.camera.getYPosition() + 30)
+        if keys[pygame.K_z]:
+            self.camera.setPosition(self.camera.getXPosition(), self.camera.getYPosition() - 30)
+        if keys[pygame.K_q]:
+            self.camera.setPosition(self.camera.getXPosition() - 30, self.camera.getYPosition())
+        if keys[pygame.K_d]:
+            self.camera.setPosition(self.camera.getXPosition() + 30, self.camera.getYPosition())
+
+    def isRunning(self):
+        return self.runningState
+            
+game = Game()
+game.main()
+
  
  
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == KEYUP:
-            if event.key == K_ESCAPE:
-                pygame.quit()
-                sys.exit()
-    pygame.display.flip()
-    FPSCLOCK.tick(30)
--- mapHandler.py
+++ mapHandler.py
@@ -1,158 +1,188 @@
 import pygame
 from pygame.locals import *
 import random
+from camera import *
+import sys
 
-
-#caracteristics
-MAP_LENGTH = 26
-MAP_WIDTH = 26
-EXT_WALLS_SIZE = 3
-PATH_SIZE = int((MAP_LENGTH * MAP_WIDTH)*3)
-
-#map data values /!\AND/!\ textures[] index
-WALL = 1
-GRASS = 0
-
-
-#data declaration   map_data = 2D list
-map_data = []
-
-
-#textures caracteristics
-TILEWIDTH       = 64  #holds the tile width and height
-TILEHEIGHT      = 64
-TILEHEIGHT_HALF = TILEHEIGHT /2
-TILEWIDTH_HALF  = TILEWIDTH /2
-
-
-#here will be stored the loaded images
-textures = []
-
-def map_init():
+class mapHandler:
     
-    #   map example
-    #   GRASS = 0   &   WALL = 1 
-
-    """"
-    map_data = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    ]  
-    """
     
-    #map data initialisation
-    for row in range(MAP_WIDTH):
-        row = []
-        for row_nb in range(MAP_LENGTH):
-            row.append(1)
+    def __init__(self):
+
+        #caracteristics
+        self.MAP_LENGTH = 300
+        self.MAP_WIDTH = 300
+        self.EXT_WALLS_SIZE = 1
+        self.PATH_SIZE = int(((self.MAP_LENGTH * self.MAP_WIDTH)))
+
+        #map data values /!\AND/!\ textures[] index
+        self.WALL = 1
+        self.GRASS = 0
+
+
+        #data declaration   map_data = 2D list
+        self.map_data = []
+
+
+        #textures caracteristics
+        self.TILEWIDTH       = 64  #holds the tile width and height
+        self.TILEHEIGHT      = 64
+        self.TILEHEIGHT_HALF = self.TILEHEIGHT /2
+        self.TILEWIDTH_HALF  = self.TILEWIDTH /2
+
+
+        #here will be stored the loaded images
+        self.textures = []
+        
+        self.map_areas_init()
+        
+
+    def map_areas_init(self):
+        
+        #   map example
+        #   GRASS = 0   &   WALL = 1 
+
+        """"
+        map_data = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        ]  
+        """
+        
+        #map data initialisation
+        for row in range(self.MAP_WIDTH):
+            row = []
+            for row_nb in range(self.MAP_LENGTH):
+                row.append(1)
+                
+            self.map_data.append(row)
             
-        map_data.append(row)
+        self.pathMaking() 
+    
+    
+    #pathMaking algorithm
+    #creates the navigable area on the map
+    def pathMaking(self):
+        row = int(self.MAP_WIDTH/2)
+        row_nb = int(self.MAP_LENGTH/2)
         
-    pathMaking() 
-    
-    
-#pathMaking algorithm
-#creates the navigable area on the map
-def pathMaking():
-    row = int(MAP_WIDTH/2)
-    row_nb = int(MAP_LENGTH/2)
-    
-    map_data[int(MAP_WIDTH/2)][int(MAP_LENGTH/2)] = GRASS
-    
-    for PATH_iterator in range(PATH_SIZE):
-        row, row_nb = step(row, row_nb)
+        self.map_data[int(self.MAP_WIDTH/2)][int(self.MAP_LENGTH/2)] = self.GRASS
         
+        self.path(row, row_nb)
+        
+        
+    def path(self, row, row_nb):
+        path_size = self.PATH_SIZE
 
-#moves the position of the path algorithm in a coherent direction
-def step(row, row_nb):
-    
-    UP = 1
-    DOWN = 2
-    LEFT = 3
-    RIGHT = 4
-    
-
-    while True:
+        for PATH_iterator in range(path_size):
+            if (PATH_iterator%10 == 0):
+                for i in range(12):
+                    row, row_nb = self.step(row, row_nb)
+            row, row_nb = self.step(row, row_nb)
         
-        PATHChoice = random.randint(1, 4)           #   1 = up      2 = down    3 = left    4 = right
-        
-        if PATHChoice == UP:
-            row += 1           
-        elif PATHChoice == DOWN:
-            row -= 1        
-        elif PATHChoice == LEFT:
-            row_nb -= 1        
-        else:
-            row_nb += 1
             
-        #if the current path position is not into the exterior walls
-        if row != (0 + EXT_WALLS_SIZE) and row != (MAP_WIDTH-EXT_WALLS_SIZE) and row_nb != (0 + EXT_WALLS_SIZE) and row_nb != (MAP_LENGTH-EXT_WALLS_SIZE):
-            break
-        else:
+
+    #moves the position of the path algorithm in a coherent direction
+    def step(self, row, row_nb):
+        
+        UP = 1
+        DOWN = 2
+        LEFT = 3
+        RIGHT = 4
+        
+
+        while True:
+            
+            PATHChoice = random.randint(1, 4)           #   1 = up      2 = down    3 = left    4 = right
+            
             if PATHChoice == UP:
-                row -= 1
+                row += 1           
             elif PATHChoice == DOWN:
-                row += 1
+                row -= 1        
             elif PATHChoice == LEFT:
-                row_nb += 1 
+                row_nb -= 1        
             else:
-                row_nb -= 1
+                row_nb += 1
+                
+            #if the current path position is not into the exterior walls
+            if row != (0 + self.EXT_WALLS_SIZE) and row != (self.MAP_WIDTH - self.EXT_WALLS_SIZE) and row_nb != (0 + self.EXT_WALLS_SIZE) and row_nb != (self.MAP_LENGTH - self.EXT_WALLS_SIZE):
+                break
+            else:
+                if PATHChoice == UP:
+                    row -= 1
+                elif PATHChoice == DOWN:
+                    row += 1
+                elif PATHChoice == LEFT:
+                    row_nb += 1 
+                else:
+                    row_nb -= 1
 
-    map_data[row][row_nb] = GRASS
-    
-    return row, row_nb;
-    
-    
-def printMap():
-    for row in range(MAP_WIDTH):
-        print(map_data[row][:])     
+        self.map_data[row][row_nb] = self.GRASS
+        
+        return row, row_nb;
+        
+        
+    def printMap(self):
+        for row in range(self.MAP_WIDTH):
+            print(self.map_data[row][:])     
 
-def getMap():
-    return map_data    
+    def getMap(self):
+        return self.map_data    
 
-#converts the 2D cartesian map to an isometric one and displays it
-def displayMap(WINDOW):
-    
-    #iterates throught the map data
-    for row_nb, row in enumerate(map_data):    
-        for col_nb, tile in enumerate(row):
-            
-            #choosing the texture to display
-            if tile == WALL:
-                tileImage = textures[WALL]
-            elif tile == GRASS:
-                tileImage = textures[GRASS]
-            
-            #Cartesian position of the texture
-            cart_x = row_nb * TILEWIDTH_HALF
-            cart_y = col_nb * TILEHEIGHT_HALF  
-            
-            #Isometric position of the texture
-            iso_x  = (cart_x - cart_y) 
-            iso_y  = (cart_x + cart_y)/2
-            
-            #pos = center of the texture
-            centered_x = WINDOW.get_rect().centerx + iso_x
-            centered_y = WINDOW.get_rect().centery/2 + iso_y
-            
-            #displays the texture
-            WINDOW.blit(tileImage, (centered_x, centered_y))   #display the actual tile
+    #converts the 2D cartesian map to an isometric one and displays it
+    def displayMap(self, WINDOW, camera):
+        
+        WINDOW.fill((0, 0, 0))
+        
+        #!TODO search into the map using camera coordinates
+        #iterates throught the map data
+        for row_nb, row in enumerate(self.map_data):    
+            for col_nb, tile in enumerate(row):
+                
+                #Cartesian position of the texture
+                cart_x = row_nb * self.TILEWIDTH_HALF
+                cart_y = col_nb * self.TILEHEIGHT_HALF  
+                
+                #Isometric position of the texture
+                iso_x  = (cart_x - cart_y) 
+                iso_y  = (cart_x + cart_y)/2
+                
+                
+                if iso_x >= int(camera.getXPosition() - (1920/2 + self.TILEWIDTH_HALF)) and iso_y >= int(camera.getYPosition() - (1080/2 + self.TILEHEIGHT_HALF)):
+                    
+                    if iso_x <= int(camera.getXPosition() + 1920/2) and  iso_y <= int(camera.getYPosition() + 1080/2):
+                        #choosing the texture to display
+                        if tile == self.WALL:
+                            tileImage = self.textures[self.WALL]
+                        elif tile == self.GRASS:
+                            tileImage = self.textures[self.GRASS]
+                        
+                        #pos = center of the texture
+                        centered_x = WINDOW.get_rect().centerx + iso_x
+                        centered_y = WINDOW.get_rect().centery/2 + iso_y
+                        
+                        #displays the texture    
+                        #31 30
+                        
+                        WINDOW.blit(tileImage, (centered_x - camera.getXPosition(), centered_y - camera.getYPosition() + 300))  
+                    elif iso_x > int(camera.getXPosition() + 1920/2) and  iso_y > int(camera.getYPosition() + 1080/2):
+                        break
 
-def loadTextures():
-    
-    #load each textures
-    wall_texture  = pygame.image.load('Assets/wall.png').convert_alpha() 
-    grass_texture = pygame.image.load('Assets/grass.png').convert_alpha()
-    
-    #add each textures to the textures list
-    textures.append(grass_texture)
-    textures.append(wall_texture)
+    def loadTextures(self):
+        
+        #load each textures
+        wall_texture  = pygame.image.load('Assets/wall.png').convert_alpha() 
+        grass_texture = pygame.image.load('Assets/grass.png').convert_alpha()
+        
+        #add each textures to the textures list
+        self.textures.append(grass_texture)
+        self.textures.append(wall_texture)
